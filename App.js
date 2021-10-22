723d73eae0ba4ab2406ea4f1b50219b7c21892cc
unknown <uzairaslam490@gmail.com>
2021-10-22 20:51:04 +0500

    initial comment

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World! </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 200,
+  },
+  text: {
+    fontFamily: 'sans-sherif',
+    fontSize: 20,
+    color: 'blue',
+  },
+});
+
+export default App;
